c802e5ac6927021f11c964a4fece37e644623a49
BearForOdin <selasidzeamesi82@gmail.com>
2025-10-05 08:21:11 -0700

    corr2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,4 +1,3 @@
-
 import unittest
 from simple_calculator import SimpleCalculator
 
@@ -20,7 +19,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test multiplication method"""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
@@ -33,3 +32,6 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.divide(-6, 3), -2)
         self.assertIsNone(self.calc.divide(5, 0))  # division by zero case
 
+
+if __name__ == '__main__':
+    unittest.main()
